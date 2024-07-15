6ad77b2b64834b4958afb7beb3246f4f4324ca27
sueyoungshim <sueyoungshim@berkeley.edu>
2024-07-14 22:49:09 -0400

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Sue Shim on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
